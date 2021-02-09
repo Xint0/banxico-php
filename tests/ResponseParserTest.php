40e40529da22ed1d602b72c6a8baa9c23bcc9634
Rogelio Jacinto <rogelio.jacinto@gmail.com>
2021-02-08 23:03:21 -0700

    test: Verify ResponseParser@parse method throws expected exception when response get contents fails or JSON parsing fails.

--- tests/ResponseParserTest.php
+++ tests/ResponseParserTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Xint0\BanxicoPHP\Tests;
 
-use Psr\Http\Message\ResponseInterface;
 use Xint0\BanxicoPHP\ClienteBanxicoException;
+use JsonException;
+use Psr\Http\Message\ResponseInterface;
 use Xint0\BanxicoPHP\ResponseParser;
+use RuntimeException;
+use Psr\Http\Message\StreamInterface;
 use PHPUnit\Framework\TestCase;
 
 class ResponseParserTest extends TestCase
@@ -18,7 +21,32 @@ class ResponseParserTest extends TestCase
         $sut = new ResponseParser();
         $this->expectException(ClienteBanxicoException::class);
         $this->expectExceptionCode(500);
-        $this->expectExceptionMessage('Response parse failed.');
+        $this->expectExceptionMessage('Request failed.');
         $sut->parse($stub);
+    }
+
+    public function test_parse_method_throws_expected_exception_when_stream_get_contents_fails(): void
+    {
+        $stubStream = $this->createStub(StreamInterface::class);
+        $runtimeException = new RuntimeException('Could not read stream.');
+        $stubStream->method('getContents')->willThrowException($runtimeException);
+        $stubResponse = $this->createStub(ResponseInterface::class);
+        $stubResponse->method('getStatusCode')->willReturn(200);
+        $stubResponse->method('getBody')->willReturn($stubStream);
+        $sut = new ResponseParser();
+        $this->expectExceptionObject(new ClienteBanxicoException('Could not get response content.', 1, $runtimeException));
+        $sut->parse($stubResponse);
+    }
+
+    public function test_parse_method_throws_expected_exception_when_stream_is_not_valid_json(): void
+    {
+        $stubStream = $this->createStub(StreamInterface::class);
+        $stubStream->method('getContents')->willReturn('not valid json');
+        $stubResponse = $this->createStub(ResponseInterface::class);
+        $stubResponse->method('getStatusCode')->willReturn(200);
+        $stubResponse->method('getBody')->willReturn($stubStream);
+        $sut = new ResponseParser();
+        $this->expectExceptionObject(new ClienteBanxicoException('Response parsing failed.', 2, new JsonException()));
+        $sut->parse($stubResponse);
     }
 }
