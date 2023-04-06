deda480ce1a148767273502e5c7fec5615e27739
Rogelio Jacinto <rogelio@elabmexico.com>
2023-04-05 23:03:52 -0600

    refactor: use Psr18ClientDiscovery instead of HttpClientDiscovery in HttpClientFactory

--- tests/Factories/HttpClientFactoryTest.php
+++ tests/Factories/HttpClientFactoryTest.php
@@ -14,9 +14,9 @@ declare(strict_types=1);
 
 namespace Xint0\BanxicoPHP\Tests\Factories;
 
-use Http\Client\HttpClient;
 use Http\Discovery\ClassDiscovery;
 use Http\Discovery\Strategy\MockClientStrategy;
+use Psr\Http\Client\ClientInterface;
 use Xint0\BanxicoPHP\Factories\HttpClientFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -27,7 +27,7 @@ class HttpClientFactoryTest extends TestCase
         ClassDiscovery::appendStrategy(MockClientStrategy::class);
         $token = 'test-token';
         $httpClient = HttpClientFactory::create($token);
-        static::assertNotNull($httpClient);
-        static::assertInstanceOf(HttpClient::class, $httpClient);
+        $this->assertNotNull($httpClient);
+        $this->assertInstanceOf(ClientInterface::class, $httpClient);
     }
 }
